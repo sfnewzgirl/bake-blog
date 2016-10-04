84745bda44147199804716304f906a9aea331300
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-04 14:23:10 -0700

    sets up bcrypt and new form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/", to: "users#index"
   get "/signup", to: "users#new"
+  post "/users", to: "users#create"
 
   get "/login", to: "sessions#new"
   # some route to update session
